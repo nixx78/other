8524c7181a4fd1d326622c2179bd751fe989ea0e
nixx78 <nixx78@gmail.com>
2024-08-29 11:26:29 +0300

    Factory method sample added

--- DesignPatterns/src/main/java/lv/nixx/poc/patterns/creational/FactoryMethod/FactoryMethod.java
+++ DesignPatterns/src/main/java/lv/nixx/poc/patterns/creational/FactoryMethod/FactoryMethod.java
@@ -1,12 +1,55 @@
 package lv.nixx.poc.patterns.creational.FactoryMethod;
 
-/*
- * Фабричный метод (англ. Factory Method также известен как Виртуальный конструктор (англ. Virtual Constructor)) 
- * порождающий шаблон проектирования, предоставляющий подклассам интерфейс для создания экземпляров некоторого класса.
- * В момент создания наследники могут определить, какой класс создавать. Иными словами, Фабрика делегирует 
- * создание объектов наследникам родительского класса. Это позволяет использовать в коде программы не 
- * специфические классы, а манипулировать абстрактными объектами на более высоком уровне.
- */
+
+interface Vehicle {
+    void drive();
+}
+
+class Car implements Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("Driving a car...");
+    }
+}
+
+class Motorcycle implements Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("Riding a motorcycle...");
+    }
+}
+
+abstract class VehicleFactory {
+    public abstract Vehicle createVehicle();
+
+    public void testDrive() {
+        Vehicle vehicle = createVehicle();
+        vehicle.drive();
+    }
+}
+
+class CarFactory extends VehicleFactory {
+    @Override
+    public Vehicle createVehicle() {
+        return new Car();
+    }
+}
+
+class MotorcycleFactory extends VehicleFactory {
+    @Override
+    public Vehicle createVehicle() {
+        return new Motorcycle();
+    }
+}
+
 public class FactoryMethod {
+
+    public static void main(String[] args) {
+        VehicleFactory carFactory = new CarFactory();
+        carFactory.testDrive();
+
+        VehicleFactory motorcycleFactory = new MotorcycleFactory();
+        motorcycleFactory.testDrive();
+    }
 
 }
